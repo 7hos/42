aad9749cc8890e4878e7ff4fc4e2e3136538e145
tanveerhossainx <tanveer.hossain.career@gmail.com>
2023-11-15 21:10:50 +0000

    Makefile added

--- 42-cursus/libft/libft.h
+++ 42-cursus/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: thossain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 20:58:01 by thossain          #+#    #+#             */
-/*   Updated: 2023/11/06 21:08:53 by thossain         ###   ########.fr       */
+/*   Updated: 2023/11/15 20:37:54 by thossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -20,9 +20,19 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_isascii(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_strncmp(const char *str1, const char *str2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_atoi(const char *str);
 void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *b, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-
+void	*ft_memchr(const void *s, int c, size_t n);
+size_t	ft_strlcpy(char *destination, const char *source, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *big, const char *little, size_t n);
 #endif
