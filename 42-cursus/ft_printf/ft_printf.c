c982e878cbed93f6f152d618229ed45eedee662e
tanveerhossainx <tanveer.hossain.career@gmail.com>
2023-12-12 00:15:25 +0000

    add putchar on printf

--- 42-cursus/ft_printf/ft_printf.c
+++ 42-cursus/ft_printf/ft_printf.c
@@ -6,10 +6,23 @@
 /*   By: thossain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 16:26:33 by thossain          #+#    #+#             */
-/*   Updated: 2023/12/07 16:27:20 by thossain         ###   ########.fr       */
+/*   Updated: 2023/12/11 23:37:08 by thossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-int	ft_printf(const char *, ...)
+#include <stdarg.h>
+/*
+• %c Prints a single character.
+• %s Prints a string (as defined by the common C convention).
+• %p The void * pointer argument has to be printed in hexadecimal format.
+• %d Prints a decimal (base 10) number.
+• %i Prints an integer in base 10.
+• %u Prints an unsigned decimal (base 10) number.
+• %x Prints a number in hexadecimal (base 16) lowercase format.
+• %X Prints a number in hexadecimal (base 16) uppercase format.
+• %% Prints a percent sign.
+*/
+
+int	ft_printf(const char *format, ...)
 {
 	
 }
