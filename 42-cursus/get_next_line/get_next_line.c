6c61bb8fc6304187ec31bdd5a73ca2afbc3cce0f
tanveerhossainx <tanveer.hossain.career@gmail.com>
2024-02-15 19:21:03 +0000

    main must be delited

--- 42-cursus/get_next_line/get_next_line.c
+++ 42-cursus/get_next_line/get_next_line.c
@@ -106,3 +106,24 @@ char	*get_next_line(int fd)
 	buffer = ft_next(buffer);
 	return (line);
 }
+
+/*
+int	main(void)
+{
+	char	*line;
+	int		i;
+	int		fd1;
+	
+	fd1 = open("tests/example.txt", O_RDONLY);
+	i = 1;
+	while (i < 7)
+	{
+		line = get_next_line(fd1);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	close(fd1);
+	return (0);
+}
+*/
